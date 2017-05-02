a9607b923729676925f60f5df6d9608dcc45f7d6
Roger <rogerwibrew@gmail.com>
2017-05-02 08:41:27 +0000

    Include and source directories added

---
+++ include/Integrator.hpp
@@ -0,0 +1,14 @@
+//
+// Created by roger on 01/05/2017.
+//
+
+#ifndef MONTECARLO2_INTEGRATOR_HPP
+#define MONTECARLO2_INTEGRATOR_HPP
+
+
+class Integrator {
+
+};
+
+
+#endif //MONTECARLO2_INTEGRATOR_HPP
